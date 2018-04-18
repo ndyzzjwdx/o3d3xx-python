8d87d0f90022c090ed9e8ff27768c972d1572dcf
ndyzzjwdx <33335251+ndyzzjwdx@users.noreply.github.com>
2018-04-18 12:56:54 +0800

    dev-0.0.1-use
    try to use

--- setup.py
+++ setup.py
@@ -2,17 +2,18 @@ from setuptools import setup, find_packages
 
 # Workaround for issue
 # https://bugs.python.org/issue15881
+
 try:
 	import multiprocessing
 except ImportError:
 	pass
 
 setup(name = 'o3d3xx',
-      version = '0.1',
-      description = 'A Python library for ifm O3D3xx devices',
-      url = 'https://github.com/cfreundl/o3d3xx-python',
-      author = 'Christoph Freundl',
-      author_email = 'Christoph.Freundl@ifm.com',
+      version = '0.1.0',
+      description = 'A Python library for ifm O3D3xx devices, created by Christoph Freundl, fixed by ndyzzjwdx',
+      url = 'https://github.com/ndyzzjwdx/o3d3xx-python',
+      author = 'ndyzzjwdx',
+      author_email = '371974021@qq.com',
       license = 'MIT',
       packages = ['o3d3xx', 'o3d3xx.rpc', 'o3d3xx.pcic'],
       install_requires = ['future'],
---
+++ work/hello.py
@@ -0,0 +1,39 @@
+#/usr/bin/python
+
+import o3d3xx
+import sys
+import time
+
+def main():
+	if len(sys.argv) > 1:
+		address=sys.argv[1]
+	else:
+		address='192.168.0.69'
+	print('IP:' + str(address))
+	
+	session = createSession(address)
+	app = createApp(session)
+	
+def createSession(address):
+	try:
+		device = o3d3xx.Device(address)
+		session = device.requestSession()
+		session.startEdit()
+	except:
+		print('session cannot be created, you must choose an available camera IP.')
+		sys.exit(1)
+	print('session can be created.')
+	return session
+	
+def createApp(session):
+	try:
+		applicationIndex = session.edit.createApplication()
+		application = session.edit.editApplication(applicationIndex)
+	except:
+		print('application cannot be created, you must choose an available camera IP.')
+		sys.exit(1)
+	print('application can be created.')
+	return application	
+		
+if __name__ == '__main__':
+	main()
